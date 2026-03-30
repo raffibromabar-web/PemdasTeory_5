67dbbbffe17d2648a624e369609370bbbbd99753
raffibromabar-web <raffibromabar@gmail.com>
2026-03-30 20:42:15 +0700

    tambahkan  kondisi lulus tatau tidak

--- looping.cpp
+++ looping.cpp
@@ -45,6 +45,9 @@ int main() {
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
 int main() {
     float nilB, nilM, rerata;
     string status;
@@ -56,5 +59,12 @@ int main() {
 
     rerata = (nilB + nilM) / 2;
 
-    
+    if (!(rerata >= 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout << "Status kelulusan = " << status << " ,dengan nilai rata-rata = " << rerata << endl;
+
+    return 0;
 }
