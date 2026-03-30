0ae99b7a616e7c97319be4371461ca20996045a8
raffibromabar-web <raffibromabar@gmail.com>
2026-03-30 20:28:37 +0700

    perulangan

--- looping.cpp
+++ looping.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main() {
     int x;
 
-    
+    // Perulangan dengan FOR
+    // Mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+
+   
 }
